7a7f30bfa875d13685def2d87827349f68bc7838
nai1gun <djkp9ik@gmail.com>
2016-10-13 16:57:50 +0200

    [/] list of links

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"regexp"
 )
 
 var states = []string {}
+var links = map[string]string {}
 
 func readStates() {
 	b, _ := ioutil.ReadFile("ROOT_STATES")
@@ -30,10 +32,67 @@ func visit(path string, f os.FileInfo, _ error) error {
 	return nil
 }
 
+func getUiSrefLinks(path string, f os.FileInfo, _ error) error {
+	if (!f.IsDir()) {
+		b, _ := ioutil.ReadFile(path)
+		content := string(b)
+		if (strings.HasSuffix(f.Name(), ".html") && strings.Contains(content, "ui-sref=")) {
+			re := regexp.MustCompile("\"([^\"]+)\"")
+			splitted := strings.Split(content, "ui-sref=")
+			for _, part := range splitted {
+				rm := re.FindStringSubmatch(part)
+				if len(rm) != 0 {
+					links[rm[1]] = rm[1] + "(ui-sref)"
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func getHtmlStateLinks(path string, f os.FileInfo, _ error) error {
+	if (!f.IsDir()) {
+		b, _ := ioutil.ReadFile(path)
+		content := string(b)
+		if (strings.HasSuffix(f.Name(), ".html") && strings.Contains(content, ".state(")) {
+			re := regexp.MustCompile("\\('([^\"]+)'")
+			splitted := strings.Split(content, ".state")
+			for _, part := range splitted {
+				rm := re.FindStringSubmatch(part)
+				if len(rm) != 0 {
+					links[rm[1]] = rm[1] + "(html-state)"
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func getStateGoLinks(path string, f os.FileInfo, _ error) error {
+	if (!f.IsDir()) {
+		b, _ := ioutil.ReadFile(path)
+		content := string(b)
+		if (strings.HasSuffix(f.Name(), ".js") && !strings.HasSuffix(f.Name(), "_test.js") && strings.Contains(content, "$state.go")) {
+			re := regexp.MustCompile("\\(\\s?'([^']+)'")
+			splitted := strings.Split(content, "$state.go")
+			for _, part := range splitted {
+				rm := re.FindStringSubmatch(part)
+				if len(rm) != 0 {
+					links[rm[1]] = rm[1] + "(state)"
+				}
+			}
+		}
+	}
+	return nil
+}
 
 func main() {
 	readStates()
 	root := "..\\unity-client\\src\\main\\angular" // Root folder for the search
-	err := filepath.Walk(root, visit)
-	fmt.Printf("filepath.Walk() returned %v\n", err)
+	filepath.Walk(root, getUiSrefLinks)
+	filepath.Walk(root, getStateGoLinks)
+	filepath.Walk(root, getHtmlStateLinks)
+	for _, link := range links {
+		fmt.Println(link)
+	}
 }
