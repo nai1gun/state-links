598b951b8675cccbb936a781afa80c3e4519ac09
nai1gun <djkp9ik@gmail.com>
2016-11-07 18:32:25 +0100

    [+] ui-sref and .state

--- main1.go
+++ main1.go
@@ -67,7 +67,7 @@ func (states States) Get(stateName string) *State {// return pointer because str
 type Views map[string]View
 
 type View struct {
-	TenplateUrl string
+	TemplateUrl string
 	Controller string
 	ControllerAs string
 }
@@ -75,6 +75,8 @@ type View struct {
 type FilesMap map[string]string
 
 type Controllers map[string]string //map[controllerName, content]
+
+type Templates map[string]string //map[templateUrl, content]
 
 func readAllStates() States {
 	b, _ := ioutil.ReadFile("all_states.json")
@@ -180,6 +182,15 @@ func readAllFiles() FilesMap {
 	return filesMap
 }
 
+func findReferencedStates(content string, reg *regexp.Regexp) StringSet {
+	submatch := reg.FindAllStringSubmatch(content, -1)
+	var states = StringSet {}
+	for _, submatchItem := range submatch {
+		states.Put(submatchItem[1])
+	}
+	return states
+}
+
 func findControllerContent(filesMap FilesMap, controllerName string) string {
 	for _, content := range filesMap {
 		if (strings.Contains(content, ".controller('" + controllerName + "'")) {
@@ -220,8 +231,9 @@ func findStateGo(allStates States, stateNames StringSet, filesMap FilesMap) Stri
 					fmt.Printf("Couldn't find any controllers for state '%s'\n", st.Name)
 				} else {
 					for _, content := range controllers {
-						goReferenceStates := findStateGosForControllerContent(content)
-						for _, reference := range goReferenceStates {
+						referencedStates := findReferencedStates(content,
+							regexp.MustCompile(`\$state\.go\('(?P<State>.+)'`))
+						for _, reference := range referencedStates {
 							if (allStates.Contains(reference) && !stateNames.Contains(reference)) {
 								stateNames.Put(reference)
 								newState := allStates.Get(reference)
@@ -237,14 +249,63 @@ func findStateGo(allStates States, stateNames StringSet, filesMap FilesMap) Stri
 	return stateNames
 }
 
-func findStateGosForControllerContent (content string) StringSet {
-	re := regexp.MustCompile(`\$state\.go\('(?P<State>.*)'`)
-	submatch := re.FindAllStringSubmatch(content, -1)
-	var states = StringSet {}
-	for _, submatchItem := range submatch {
-		states.Put(submatchItem[1])
+func findStateTemplates(state State, filesMap FilesMap) Templates {
+	var templates = Templates{}
+	getContent := func(filesMap FilesMap, templateUrl string) {
+		systemTemplateUrl := fixSeparator(templateUrl)
+		templateContent := filesMap[systemTemplateUrl]
+		templates[systemTemplateUrl] = templateContent
 	}
-	return states
+	if (state.TemplateUrl != "") {
+		getContent(filesMap, state.TemplateUrl)
+	}
+	for _, view := range state.Views {
+		if (view.TemplateUrl != "") {
+			getContent(filesMap, view.TemplateUrl)
+		}
+	}
+	return templates
+}
+
+func fixSeparator(path string) string {
+	return strings.Replace(path, "/", systemSeparator, -1)
+}
+
+func findUiSref(allStates States, stateNames StringSet, filesMap FilesMap) StringSet {
+	return findInTemplates(allStates, stateNames, filesMap, regexp.MustCompile(`ui-sref="(?P<State>.+?)[\("]`))
+}
+
+
+func findStateRef(allStates States, stateNames StringSet, filesMap FilesMap) StringSet {
+	return findInTemplates(allStates, stateNames, filesMap, regexp.MustCompile(`.state\('(?P<State>.+?)'`))
+}
+
+func findInTemplates(allStates States, stateNames StringSet, filesMap FilesMap, reg *regexp.Regexp) StringSet {
+	for _, state := range allStates {
+		if (stateNames.Contains(state.Name)) {
+			findStateTemplates(state, filesMap)
+			var findInState func(State)
+			findInState = func(st State) {
+				var templates = findStateTemplates(st, filesMap)
+				if (len(templates) == 0) {
+					fmt.Printf("Couldn't find any templates for state '%s'\n", st.Name)
+				} else {
+					for _, content := range templates {
+						referencedStates := findReferencedStates(content, reg)
+						for _, reference := range referencedStates {
+							if (allStates.Contains(reference) && !stateNames.Contains(reference)) {
+								stateNames.Put(reference)
+								newState := allStates.Get(reference)
+								findInState(*newState) //find recursively
+							}
+						}
+					}
+				}
+			}
+			findInState(state)
+		}
+	}
+	return stateNames
 }
 
 func main() {
@@ -255,6 +316,8 @@ func main() {
 
 	filesMap := readAllFiles()
 	findStateGo(states, stateNamesToKeep, filesMap)
+	findUiSref(states, stateNamesToKeep, filesMap)
+	findStateRef(states, stateNamesToKeep, filesMap)
 
 	var stateNamesList = []string {}
 	for s := range stateNamesToKeep {
